cf3be55dc583da9af43af94a331a88a0225adb14
Hugo Ho <hugoho912@outlook.com>
2022-10-31 13:14:30 +0800

    第二次作业完成✅

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 //        homework1 hw1Test = new homework1();
@@ -7,7 +9,9 @@ public class Main {
 //        hw2Test.inTri();
 //        hw2Test.area();
         homework21 hw21Test = new homework21();
-//        hw21Test.calISBN();
-        hw21Test.printFutureInvestmentValue();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a card number: ");
+        long number = sc.nextLong();
+        System.out.println(hw21Test.isValid(number) ? "Valid" : "Invalid");
     }
 }
--- src/homework21.java
+++ src/homework21.java
@@ -30,15 +30,16 @@ public class homework21 {
     }
 
     // 第二题
-    public static double futureInvestmentValue(
-            double investmentAmount, double monthlyInterestRate, int years) {
+    public static double futureInvestmentValue(double investmentAmount, double monthlyInterestRate, int years) {
         return investmentAmount * Math.pow(1 + monthlyInterestRate, years * 12);
     }
+
     public static String format1(double value) {
         BigDecimal bd = new BigDecimal(value);
         bd = bd.setScale(2, RoundingMode.HALF_UP);
         return bd.toString();
     }
+
     public static void printFutureInvestmentValue() {
         Scanner sc = new Scanner(System.in);
         System.out.println("The amount invested: ");
@@ -52,20 +53,74 @@ public class homework21 {
     }
 
     // 第三题
-    public static boolean isValid(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a credit card number as a long integer: ");
-        long input = sc.nextLong();
-        // 对每个数字翻倍，是两位数则调用sumOfDoubleDigit
-        for (int i = 0; i < String.valueOf(input).length();){
+//    public static boolean isValid(){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a credit card number as a long integer: ");
+//        long input = sc.nextLong();
+//        // 对每个数字翻倍，是两位数则调用sumOfDoubleDigit
+//        for (int i = 0; i < String.valueOf(input).length();){
+//            if (i % )
+//        }
+//    };
+    public static boolean isValid(long number) {
+        //检测前缀，检测是否可以整除10，检测位数是否正确
+        if ((getSize(number) >= 13 && getSize(number) <= 16) && (prefixMatched(number, 4) || prefixMatched(number, 5) || prefixMatched(number, 37) || prefixMatched(number, 6)) && ((sumOfDoubleEvenPlace(number) + sumOfOddPlace(number)) % 10 == 0))
+            return true;
+        else return false;
+    }
 
+
+    public static int sumOfDoubleEvenPlace(long number) {
+        int sum = 0;
+        long number1 = number;
+        for (int i = 1; i <= getSize(number); i += 1) {
+            sum += getDigit((int) (number1 % 10));
+            number1 /= 10;
+        }
+        return sum;
+    }
+
+    //返回一个两位数上的两个数之和或者原数
+    public static int getDigit(int number) {
+        if (number < 10)
+            return number;
+        else
+            return number / 10 + number % 10;
+    }
+
+    //返回奇数位的和
+    public static int sumOfOddPlace(long number) {
+        int sum = 0;
+        long number1 = number;
+        //从第一位开始，遍历奇数位
+        for (int i = 1; i <= getSize(number); i += 2) {
+            sum += number1 % 10;
+            number1 /= 100;
+        }
+        return sum;
+    }
+
+    public static boolean prefixMatched(long number, int d) {
+        return d == getPrefix(number, getSize(d));//判断相等
+    }
+
+    //得到整数长度
+    public static int getSize(long d) {
+        int i = 1;
+        while (true) {
+            if (d / 10 == 0)
+                return i;
+            else
+                d = d / 10;
+            i++;
         }
     }
 
-    public static int sumOfDoubleEvenPlace(long number){}
-    public static int getDigit(int number){}
-    public static int sumOfOddPlace(long number){}
-    public static boolean prefixMatched(long number, int d){}
-    public static int getSize(long d){}
-    public static long getPrefix(long number, int k){}
+    //返回前k个数
+    public static long getPrefix(long number, int k) {
+        if (getSize(number) < k)
+            return number;
+        long prefixNumber = number / (long) Math.pow(10, getSize(number) - k);
+        return prefixNumber;
+    }
 }
